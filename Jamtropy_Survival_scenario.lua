3d9f7a1f36ee8172bc39b58353426d1233e52c19
Jeroen De Dauw <jeroendedauw@gmail.com>
2020-05-02 22:51:54 +0200

    Create map with proof of concept circular spawner
    ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
    ░░░░░░░░░░▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄░░░░░░░░░
    ░░░░░░░░▄▀░░░░░░░░░░░░▄░░░░░░░▀▄░░░░░░░
    ░░░░░░░░█░░▄░░░░▄░░░░░░░░░░░░░░█░░░░░░░
    ░░░░░░░░█░░░░░░░░░░░░▄█▄▄░░▄░░░█░▄▄▄░░░
    ░▄▄▄▄▄░░█░░░░░░▀░░░░▀█░░▀▄░░░░░█▀▀░██░░
    ░██▄▀██▄█░░░▄░░░░░░░██░░░░▀▀▀▀▀░░░░██░░
    ░░▀██▄▀██░░░░░░░░▀░██▀░░░░░░░░░░░░░▀██░
    ░░░░▀████░▀░░░░▄░░░██░░░▄█░░░░▄░▄█░░██░
    ░░░░░░░▀█░░░░▄░░░░░██░░░░▄░░░▄░░▄░░░██░
    ░░░░░░░▄█▄░░░░░░░░░░░▀▄░░▀▀▀▀▀▀▀▀░░▄▀░░
    ░░░░░░█▀▀█████████▀▀▀▀████████████▀░░░░
    ░░░░░░████▀░░███▀░░░░░░▀███░░▀██▀░░░░░░
    ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░

---
+++ Jamtropy_Survival_scenario.lua
@@ -0,0 +1,28 @@
+version = 3 -- Lua Version. Dont touch this
+ScenarioInfo = {
+    name = "Jamtropy Survival",
+    description = "Adaptive survival map by Jammer and EntropyWins",
+    preview = '',
+    map_version = 1,
+    type = 'skirmish',
+    starts = true,
+    size = {1024, 1024},
+    reclaim = {16200, 0},
+    map = '/maps/Jamtropy_Survival.v0001/Jamtropy_Survival.scmap',
+    save = '/maps/Jamtropy_Survival.v0001/Jamtropy_Survival_save.lua',
+    script = '/maps/Jamtropy_Survival.v0001/Jamtropy_Survival_script.lua',
+    norushradius = 40,
+    Configurations = {
+        ['standard'] = {
+            teams = {
+                {
+                    name = 'FFA',
+                    armies = {'ARMY_1', 'ARMY_2', 'ARMY_3', 'ARMY_4', 'ARMY_5', 'ARMY_6', 'ARMY_7', 'ARMY_8'}
+                },
+            },
+            customprops = {
+                ['ExtraArmies'] = STRING( 'SURVIVAL_ENEMY NEUTRAL_CIVILIAN' ),
+            },
+        },
+    },
+}
---
+++ Jamtropy_Survival_script.lua
@@ -0,0 +1,68 @@
+
+function OnPopulate()
+    import('/lua/sim/ScenarioUtilities.lua').InitializeArmies()
+    import('/lua/ScenarioFramework.lua').SetPlayableArea('AREA_1' , false)
+end
+
+function OnStart(scenario)
+end
+
+local newCircularSpawner = function(unitCreator)
+    local radius = 250
+    local innerRadius = 15
+    local z = 25.9844
+
+    local center = {
+        x = ScenarioInfo.size[2] / 2,
+        y = ScenarioInfo.size[1] / 2
+    }
+
+    return {
+        spawnUnit = function(unitId, unitCount)
+            for _ = 0, unitCount or 1 do
+                local angle = math.rad(Random(0, 360))
+
+                local unit = unitCreator.create({
+                    blueprintName = unitId,
+                    armyName = "SURVIVAL_ENEMY",
+                    x = center.x + radius * math.sin(angle),
+                    y = center.y + radius * math.cos(angle),
+                    z = z
+                })
+
+                IssueMove(
+                    {unit},
+                    VECTOR3(
+                        center.x + innerRadius * math.sin(angle),
+                        z,
+                        center.y + innerRadius * math.cos(angle)
+                    )
+                )
+            end
+        end
+    }
+end
+
+local mapPath = '/maps/Jamtropy_Survival.v0001/'
+local entropyLib = import(mapPath .. 'vendor/EntropyLib/src/EntropyLib.lua').newInstance(mapPath .. 'vendor/EntropyLib/')
+local circularSpawner = newCircularSpawner(entropyLib.newUnitCreator())
+
+function OnShiftF3()
+    circularSpawner.spawnUnit("DEL0204") -- Best unit clearly
+end
+
+function OnShiftF4()
+    circularSpawner.spawnUnit("DEL0204", 100)
+end
+
+function OnShiftF5()
+    ForkThread(function()
+        local iteration = 0
+
+        while iteration < 15 do
+            circularSpawner.spawnUnit("DEL0204", 20)
+            WaitSeconds(1)
+            iteration = iteration + 1
+        end
+    end)
+end
